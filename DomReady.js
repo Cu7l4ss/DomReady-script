9cdc1b2e794b566a6368d39f9a67a9ef712db253
Shlomi Zfira <shlomizf@gmail.com>
2011-07-25 01:12:13 +0300

    Fixed the construction a bit, standalone and now can be dynamicly loaded without closure problems

--- DomReady.js
+++ DomReady.js
@@ -1,114 +1,106 @@
-var DomReady = (function(my) {
-	my = my || {};
-	var readyList = [],
-		isReady = my.isReady = false,
-		readyBound = false,
-		readyWait = 1,
-		DOMContentLoaded,
-		ready = my.ready = function (fn) {
-			
-			my.bindReady();
-			
-				
-			if ( isReady && typeof fn === "function") {
-				fn.call(document);
-			} else if ( readyList && typeof fn === "function") {
-				readyList.push(fn);
-				return;
-			} else if ( isReady && readyList ) {
+(function() {
+    var DomReady = rur = {}, isReady = false;
+    var readyBound = false;
+    rur.stack = [];
+    rur.ready = function (args) {
+        rur.bindReady();
+         if ( isReady && typeof args === "function" ) {
+             args.call(document);
+         } else if ( rur.stack && typeof args === "function" ) {
+             rur.stack.push(args);
+             return;
+         } else if (isReady && rur.stack) {
+             if (!document.body)
+                 return setTimeout(rur.ready, 1);
+            var fn, i = 0;
+            while(( fn = rur.stack[i++])) {
+                fn.call(document);
+            }    
+            
+            rur.stack = null;
+         }
+    };
+    
+    rur.bindReady = function () {
+        if(readyBound) {
+            return;
+        }
+        readyBound = true;
 
-				if ( !document.body ) {
-						return setTimeout( DomReady.ready, 1 );
-				}
-				// Execute all of them
-				var fn, i = 0;
-				while ( (fn = readyList[ i++ ]) ) {
-					fn.call( document, DomReady );
-				}
+        // Catch cases where ready() is called after the
+        // browser event has already occurred.
+        if(document.readyState === "complete") {
+            // Handle it asynchronously to allow scripts the opportunity to delay ready
+            return setTimeout(rur.ready, 1);
+        }
 
-				// Reset the list of functions
-				readyList = null;
-			}
-		},
-		bindReady = my.bindReady = function () {
-			if ( readyBound ) {
-				return;
-			}
+        // Mozilla, Opera and webkit nightlies currently support this event
+        if(document.addEventListener) {
+            // Use the handy event callback
+            document.addEventListener("DOMContentLoaded", DOMContentLoaded, false);
 
-			readyBound = true;
+            // A fallback to window.onload, that will always work
+            
+            window.addEventListener("load", rur.ready, false);
 
-			// Catch cases where DomReady.ready() is called after the
-			// browser event has already occurred.
-			if ( document.readyState === "complete" ) {
-				// Handle it asynchronously to allow scripts the opportunity to delay ready
-				return setTimeout( DomReady.ready, 1 );
-			}
+            // If IE event model is used
+        } else if(document.attachEvent) {
+            // ensure firing before onload,
+            // maybe late but safe also for iframes
+            document.attachEvent("onreadystatechange", DOMContentLoaded);
 
-			// Mozilla, Opera and webkit nightlies currently support this event
-			if ( document.addEventListener ) {
-				// Use the handy event callback
-				document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+            // A fallback to window.onload, that will always work
+            window.attachEvent("onload", rur.ready);
 
-				// A fallback to window.onload, that will always work
-				window.addEventListener( "load", DomReady.ready, false );
+            // If IE and not a frame
+            // continually check to see if the document is ready
+            var toplevel = false;
 
-			// If IE event model is used
-			} else if ( document.attachEvent ) {
-				// ensure firing before onload,
-				// maybe late but safe also for iframes
-				document.attachEvent("onreadystatechange", DOMContentLoaded);
+            try {
+                toplevel = window.frameElement == null;
+            } catch(e) {
+            }
 
-				// A fallback to window.onload, that will always work
-				window.attachEvent( "onload", DomReady.ready );
+            if(document.documentElement.doScroll && toplevel) {
+                doScrollCheck();
+            }
+        }    
+    };
+    function doScrollCheck() {
+        if(rur.isReady) {
+            return;
+        }
 
-				// If IE and not a frame
-				// continually check to see if the document is ready
-				var toplevel = false;
+        try {
+            // If IE is used, use the trick by Diego Perini
+            // http://javascript.nwbox.com/IEContentLoaded/
+            document.documentElement.doScroll("left");
+        } catch(e) {
+            setTimeout(doScrollCheck, 1);
+            return;
+        }
 
-				try {
-					toplevel = window.frameElement == null;
-				} catch(e) {}
-
-				if ( document.documentElement.doScroll && toplevel ) {
-					doScrollCheck();
-				}
-			}
-		};
-	
-	function doScrollCheck() {
-		if ( isReady ) {
-			return;
-		}
-
-		try {
-			// If IE is used, use the trick by Diego Perini
-			// http://javascript.nwbox.com/IEContentLoaded/
-			document.documentElement.doScroll("left");
-		} catch(e) {
-			setTimeout( doScrollCheck, 1 );
-			return;
-		}
-
-		// and execute any waiting functions
-		my.ready();
-	}
-	
-	if ( document.addEventListener ) {
-		DOMContentLoaded = function() {
-			document.removeEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-			isReady = my.isReady = true;
-			my.ready();
-		};
-	} else if ( document.attachEvent ) {
-		DOMContentLoaded = function() {
-		// Make sure body exists, at least, in case IE gets a little overzealous (ticket #5443).
-			if ( document.readyState === "complete" ) {
-				isReady = my.isReady = true;
-				document.detachEvent( "onreadystatechange", DOMContentLoaded );
-				my.ready();
-			}
-		};
-	}
-	
-	return my;	
-}(DomReady || {}));
+        // and execute any waiting functions
+        rur.ready();
+    }
+    
+    if(document.addEventListener) {
+        DOMContentLoaded = function() {
+            document.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
+            isReady = true;
+            rur.ready();
+        };
+    } else if(document.attachEvent) {
+        DOMContentLoaded = function() {
+            // Make sure body exists, at least, in case IE gets a little overzealous (ticket #5443).
+            if(document.readyState === "complete") {
+                isReady = true;
+                document.detachEvent("onreadystatechange", DOMContentLoaded);
+                rur.ready();
+            }
+        };
+    }
+    
+    if(!!window)
+        window.DomReady = rur;
+}());
